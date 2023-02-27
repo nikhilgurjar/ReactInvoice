56c67d4b85ff5b01786a55ed86bb112c4cb3d868
nikhilgurjar <gurjarnikhil2022@gmail.com>
2023-02-27 22:47:32 +0530

    added changes for git

---
+++ src/interfaces/invoice.ts
@@ -0,0 +1,37 @@
+import { ICompany } from './company';
+import { IPerson } from './person';
+
+export interface IInvoiceRecipient extends IPerson, ICompany {}
+
+export interface IInvoiceSender extends IPerson, ICompany {
+  panCard: string
+}
+
+export interface IInvoiceLineItem {
+  description: string;
+  quantity: string;
+  rate: string;
+  tax?: string;
+}
+
+export interface IInvoicePaymentInfo {
+  accountName?: string;
+  accountNumber?: string;
+  IFSC?: string;
+}
+
+export interface IInvoice {
+  fileName?: string;
+  logo?: string;
+  sender: IInvoiceSender;
+  recipient: IInvoiceRecipient;
+  invoiceNumber: string;
+  date: string;
+  due?: string;
+  items: Array<IInvoiceLineItem>;
+  taxRate?: number;
+  terms?: string;
+  notes?: string;
+  footerMessages?: string;
+  paymentInfo?: IInvoicePaymentInfo;
+}
